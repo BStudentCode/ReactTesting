31b0be9030533affa93ccd4828875a0e976a0354
B <hennyhexservers@gmail.com>
2021-03-16 00:26:04 +0000

    passing children
    Signed-off-by: B <hennyhexservers@gmail.com>

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,10 +11,14 @@ class Counters extends Component {
     ],
   };
   render() {
+    //Passing <h4> element as child for rendering possibly different title on each Counter on page, useful for dialogue boxes
+    //Easier way is to add another property to Counter eg id={counter.id} and wrap that in <h4> on counter.jsx render()
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter key={counter.id} value={counter.value} selected={true}>
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
         ))}
       </div>
     );
