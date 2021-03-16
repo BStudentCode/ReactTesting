f91cff32544dfb9a3799c12403b762cd01cecdda
B <hennyhexservers@gmail.com>
2021-03-16 15:08:32 +0000

    lifting the state up
    Signed-off-by: B <hennyhexservers@gmail.com>

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,55 +2,26 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-    console.log(this.state.counters[index]);
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
   render() {
     //Passing <h4> element as child for rendering possibly different title on each Counter on page, useful for dialogue boxes
     //Easier way is to add another property to Counter eg id={counter.id} and wrap that in <h4> on counter.jsx render()
 
     //Counter onDelete=this.handleDelete --- added onDelete method in Counter and it recognises it as prop here
     //key and id both passed as key is used internally by React and we cannot access it, but we can just pass counter object itself
+
+    //lifted the state up to App.js
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             key={counter.id}
             counter={counter}
             selected={true}
